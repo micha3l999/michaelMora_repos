0323eaf4f5ae2aa8eeffe17ef5971d1fca028d05
Michael Mora <maycolmora99@gmail.com>
2022-08-10 17:56:31 -0500

    Create constants

---
+++ src/helpers/constants.ts
@@ -0,0 +1,11 @@
+export const VERIFICATION_STATUS = {
+    '604': 'Verificado',
+    '605': 'En espera',
+    '606': 'Aprobado',
+}
+
+export const REPOSITORY_STATE = {
+    'E': 'Enable',
+    'D': 'Disable',
+    'A': 'Archived',
+}
