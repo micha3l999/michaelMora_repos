72f68492989990c7d0cd9719d0538118a0ad019f
Michael Mora <maycolmora99@gmail.com>
2022-08-10 18:10:21 -0500

    Adding organization service and controller

--- src/organizations/organizations.controller.ts
+++ src/organizations/organizations.controller.ts
@@ -1,23 +1,81 @@
-import { Body, Controller, Delete, Get, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
-import { get } from 'https';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    ParseIntPipe,
+    Post,
+    Put,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { BaseResponse } from 'src/common/baseResponse';
 import { Organizations } from 'src/entities';
 import { CreateOrganizationDto } from './dto/CreateOrganization.dto';
+import { UpdateOrganizationDto } from './dto/UpdateOrganization.dto';
 import { OrganizationsService } from './organizations.service';
 
 @Controller('organizations')
+@ApiTags('Organizations')
 export class OrganizationsController {
     constructor(private readonly organizationsService: OrganizationsService) {}
 
     @Post('')
-    async postOrganizations(@Body() createOrganizationDto: CreateOrganizationDto): Promise<BaseResponse<Organizations>> {
-        const newOrganization = await this.organizationsService.createOrganization(createOrganizationDto);
+    @ApiOperation({ summary: 'Create an organization' })
+    @ApiResponse({
+        status: 200,
+        description: 'Post create organization successfully',
+        type: BaseResponse<Organizations>,
+      })
+    async postOrganization(
+        @Body() createOrganizationDto: CreateOrganizationDto,
+    ): Promise<BaseResponse<Organizations>> {
+        const newOrganization =
+            await this.organizationsService.createOrganization(
+                createOrganizationDto,
+            );
         return new BaseResponse<Organizations>(newOrganization);
     }
 
     @Get('')
+    @ApiOperation({ summary: 'Get all organizations created' })
+    @ApiResponse({
+        status: 200,
+        description: 'Get successfully',
+        type: BaseResponse<Organizations[]>,
+      })
     async getOrganizations(): Promise<BaseResponse<Organizations[]>> {
-        const organizations = await this.organizationsService.getOrganizations();
+        const organizations =
+            await this.organizationsService.getOrganizations();
         return new BaseResponse<Organizations[]>(organizations);
+    }
+
+    @Put('/:idOrganization')
+    @ApiOperation({ summary: 'Update an organization by Id' })
+    @ApiResponse({
+        status: 200,
+        description: 'Update successfully',
+        type: BaseResponse<UpdateOrganizationDto>,
+      })
+    async updateOrganization(
+        @Param('idOrganization', ParseIntPipe) idOrganization: number,
+        @Body() updateOrganizationDto: UpdateOrganizationDto,
+    ): Promise<Organizations> {
+        const organization =
+            await this.organizationsService.updateOrganization(idOrganization, updateOrganizationDto);
+        return organization;
+    }
+
+    @Delete('/:idOrganization')
+    @ApiOperation({ summary: 'Delte an organization by Id' })
+    @ApiResponse({
+        status: 200,
+        description: 'Delete successfully',
+        type: BaseResponse<Organizations>,
+      })
+    async deleteOrganization(@Param('idOrganization', ParseIntPipe) idOrganization: number): Promise<Organizations> {
+        const organization =
+            await this.organizationsService.deleteOrganization(idOrganization);
+        return organization;
     }
 }
--- src/organizations/organizations.service.ts
+++ src/organizations/organizations.service.ts
@@ -3,13 +3,21 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Organizations } from 'src/entities';
 import { Repository } from 'typeorm';
 import { CreateOrganizationDto } from './dto/CreateOrganization.dto';
+import { UpdateOrganizationDto } from './dto/UpdateOrganization.dto';
 
 @Injectable()
 export class OrganizationsService {
-    constructor(@InjectRepository(Organizations) private readonly organizationsRepository: Repository<Organizations>) {}
+    constructor(
+        @InjectRepository(Organizations)
+        private readonly organizationsRepository: Repository<Organizations>,
+    ) {}
 
-    async createOrganization(createOrganizationDto: CreateOrganizationDto): Promise<Organizations> {
-        const newOrganization = this.organizationsRepository.create(createOrganizationDto);
+    async createOrganization(
+        createOrganizationDto: CreateOrganizationDto,
+    ): Promise<Organizations> {
+        const newOrganization = this.organizationsRepository.create(
+            createOrganizationDto,
+        );
         await this.organizationsRepository.save(newOrganization);
         return newOrganization;
     }
@@ -17,5 +25,33 @@ export class OrganizationsService {
     async getOrganizations(): Promise<Organizations[]> {
         const organizations = await this.organizationsRepository.find();
         return organizations;
+    }
+
+    async updateOrganization(
+        idOrganization: number,
+        updateOrganizationDto: UpdateOrganizationDto,
+    ): Promise<Organizations> {
+        const response = await this.organizationsRepository.findOne({
+            where: { id: idOrganization },
+        });
+        if (!response) {
+            throw new BadRequestException('Organization Not found');
+        }
+        await this.organizationsRepository.update(
+            idOrganization,
+            updateOrganizationDto,
+        );
+        return response;
+    }
+
+    async deleteOrganization(idOrganization: number): Promise<Organizations> {
+        const response = await this.organizationsRepository.findOne({
+            where: { id: idOrganization },
+        });
+        if (!response) {
+            throw new BadRequestException('Organization Not found');
+        }
+        await this.organizationsRepository.delete(idOrganization);
+        return response;
     }
 }
