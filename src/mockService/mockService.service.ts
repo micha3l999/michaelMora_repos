93de780fe2116b3c0b2ff64fee625c6ef3d2b46c
Michael Mora <maycolmora99@gmail.com>
2022-08-09 21:24:45 -0500

    fixes

--- src/mockService/mockService.service.ts
+++ src/mockService/mockService.service.ts
@@ -3,24 +3,22 @@ import { MockService } from 'src/common/mockService';
 
 @Injectable()
 export class MockServiceService {
-    getMockRepositories(): MockService[] {
-        return [
-            {
-                repositories: [
-                    {
-                        id: 1,
-                        state: 604,
-                    },
-                    {
-                        id: 2,
-                        state: 605,
-                    },
-                    {
-                        id: 3,
-                        state: 606,
-                    },
-                ],
-            },
-        ];
+    getMockRepositories(): MockService {
+        return {
+            repositories: [
+                {
+                    id: 1,
+                    state: 604,
+                },
+                {
+                    id: 2,
+                    state: 605,
+                },
+                {
+                    id: 3,
+                    state: 606,
+                },
+            ],
+        };
     }
 }
--- src/organizations/dto/CreateOrganization.dto.ts
+++ src/organizations/dto/CreateOrganization.dto.ts
@@ -2,7 +2,6 @@ import { IsNotEmpty, MaxLength, MinLength } from "class-validator";
 
 export class CreateOrganizationDto {
     @IsNotEmpty()
-    @MinLength(5)
     @MaxLength(50)
     name: string;
 
