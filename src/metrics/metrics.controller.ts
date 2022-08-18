c3270712b3b8936a4cf9f8016f681f40c18d84f8
Michael Mora <maycolmora99@gmail.com>
2022-08-18 17:48:00 -0500

    Added swagger documentation in the controller

--- src/metrics/metrics.controller.ts
+++ src/metrics/metrics.controller.ts
@@ -1,21 +1,35 @@
 import { Controller, Get, Injectable, Param, ParseIntPipe, Res, Response, StreamableFile } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { BaseResponse } from 'src/common/baseResponse';
 import { MetricsService } from './metrics.service';
 import { MetricsRepositoryResponse } from './models/metricsRepositoryResponse';
 
 @Controller('metrics')
 @Injectable()
+@ApiTags('Metrics')
 export class MetricsController {
     constructor(private readonly metricsService: MetricsService) {}
 
     @Get('/filterByTribe/:idTribe')
+    @ApiOperation({ summary: 'Get metrics by id tribe' })
+    @ApiResponse({
+        status: 200,
+        description: 'Get Successfully',
+        type: BaseResponse<MetricsRepositoryResponse>,
+      })
     async getMetrics(@Param('idTribe', ParseIntPipe) idTribe: number): Promise<BaseResponse<MetricsRepositoryResponse>> {
         const response = await this.metricsService.getMetrics(idTribe);
         return new BaseResponse<MetricsRepositoryResponse>(response);
     }
 
     @Get('/exportData/:idTribe')
-    async getFile(@Param('idTribe', ParseIntPipe) idTribe: number, @Response({ passthrough: true }) res: any): Promise<any> {
+    @ApiOperation({ summary: 'Get a csv file format with the metrics information' })
+    @ApiResponse({
+        status: 200,
+        description: 'Get file successfully',
+        type: BaseResponse<StreamableFile>,
+      })
+    async getFile(@Param('idTribe', ParseIntPipe) idTribe: number, @Response({ passthrough: true }) res: any): Promise<StreamableFile> {
         const response = await this.metricsService.getMetrics(idTribe);
         const filecreated = this.metricsService.generatecsvFile(response);
         res.set({
--- src/metrics/models/metricsRepositoryResponse.ts
+++ src/metrics/models/metricsRepositoryResponse.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { MetricsResponse } from "./metricsResponse";
 
 export class MetricsRepositoryResponse {
+    @ApiProperty({
+        example: '',
+        description: 'The respositories collection',
+    })
     public repositories: MetricsResponse[];
 }
